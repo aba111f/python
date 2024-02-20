7bfa0dc7cba1efceef7ddbaa5728cb8fb017a16d
aba111f <a_khamzin@mail.ru>
2024-02-20 18:36:12 +0600

    generators

--- Lab_4/generator/ex2.py
+++ Lab_4/generator/ex2.py
@@ -3,6 +3,8 @@ def even_ns(n):
         if i%2 == 0:
             yield i
 
-print(list(even_ns(5)))
-print(list(even_ns(15)))
-print(list(even_ns(29)))
+
+
+x = list(even_ns(5))
+nums_str = ', '.join(map(str,x))
+print(nums_str)
