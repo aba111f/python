5e8e4862125db4090738ffa4fa200a26fa4bcb97
aba111f <a_khamzin@mail.ru>
2024-03-04 21:07:20 +0600

    fix

--- Lab_6/Built-infunc/ex2.py
+++ Lab_6/Built-infunc/ex2.py
@@ -1,7 +1,12 @@
-def up_low(s):      
-    u = sum(1 for i in s if i.isupper())
-    l = sum(1 for i in s if i.islower())
-    print(u,l)
+def is_up_low(word):
+    up = 0
+    low = 0
+    for i in word:
+        if i.isupper():
+            up += 1
+        else:
+            low += 1
+    print(up, low)
 
-s = input()
-print(up_low(s))
+word = input()
+print(is_up_low(word))
